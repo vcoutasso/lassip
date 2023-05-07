3623a2d071dabae5e2ae731eec62774f6e071219
Vinícius Couto Tasso <vcoutasso@gmail.com>
2023-05-07 16:31:00 -0300

    wip saft

--- SAFT/saft.py
+++ SAFT/saft.py
@@ -3,35 +3,40 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    bscan = np.load("test.npy")
+    bscan = np.load("bscan.npy")
 
     n, tc = bscan.shape
-    t = np.arange(n)
+
+    height = 50
+    width = 50
+    origin = (int(height / 2), -int(width / 2))
+    resolution = (1000, tc)
+
+    z = np.linspace(origin[0] + origin[0] + height, resolution[0])
+    x = np.linspace(origin[1] + origin[1] + width, resolution[1])
+
+    dt = 5e-9
+    dx = 100e-6
+
     c = 0.35
-    roi = 250
 
-    samples_per_period = 36
-    f = 1e6
+    f = 5e6
+    samples_per_period = 30
 
-    T = f / samples_per_period
-    print(T)
+    T = samples_per_period / f
 
-    xs = np.arange(tc)
-    xs += (int(roi / 2)) - xs[int(tc / 2)]
-    z = c * t / 2
+    xt = np.arange(tc) * dx
 
-    saft = np.zeros((roi, roi))
-    indices = np.arange(roi)
-    xx, zz = np.meshgrid(indices, z)
+    saft = np.zeros((height, width))
+    xx, zz = np.meshgrid(x, z)
 
     for elem in range(tc):
-        dist = np.sqrt(np.power(xx - xs[elem], 2) + np.power(zz, 2))
-        delay = 2 * dist / c
+        dist = np.sqrt(np.power(xx - xt[elem], 2) + np.power(zz, 2))
+        delay = dt * 2 * dist / c
         sample = np.round(delay / T).astype(int)
+        print(sample)
         ascan = np.pad(bscan[:, elem], (0, 1))
         saft += np.take(ascan, sample, mode="clip")
-
-    saft /= tc
 
     plt.imshow(saft)
     plt.show()
