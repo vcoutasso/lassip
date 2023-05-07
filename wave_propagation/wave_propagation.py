baf01fd957539d92e0f1e7fef0c416d9ab41baeb
Vinícius Couto Tasso <vcoutasso@gmail.com>
2023-05-07 14:36:49 -0300

    low memory wave propagation simulation

--- wave_propagation/wave_propagation.py
+++ wave_propagation/wave_propagation.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable
 import numpy as np
 import cv2
 
@@ -6,16 +7,22 @@ class WavePropagation:
     def __init__(
         self,
         grid: np.ndarray,
-        time: np.ndarray,
+        t_max: int,
         velocity: np.ndarray,
-        transducer_pos: list,
-        source: np.ndarray,
+        transducer_pos: tuple[int, int],
+        source: Callable[[int, tuple[int, int]], np.ndarray],
+        gating: int = 350,
     ):
-        self.G = grid
-        self.t = time
+        if grid.ndim != 2:
+            raise Exception("Grid must have two spatial dimensions.")
+
+        self.G = np.tile(grid, (3, 1, 1))
+        self.t = t_max
         self.C = velocity
         self.tp = transducer_pos
         self.s = source
+        self.gating = gating
+        self.a_scan = np.zeros(t_max)
 
     def laplassian(self, i):
         G_pad = np.pad(self.G[i], (1,))
@@ -23,36 +30,46 @@ class WavePropagation:
             G_pad[1:-1, 2:] + G_pad[1:-1, :-2] + G_pad[2:, 1:-1] + G_pad[:-2, 1:-1]
         ) - 4 * self.G[i]
 
-    def simulate(self) -> np.ndarray:
-        for i in self.t[:-1]:
-            self.G[i + 1] = (
-                self.laplassian(i) * np.power(self.C, 2)
-                + 2 * self.G[i]
-                - self.G[i - 1]
-                + self.s[i, :]
+    def simulate(self, video_out: bool) -> np.ndarray:
+        if video_out:
+            out = cv2.VideoWriter(
+                "simulation.mp4",
+                cv2.VideoWriter_fourcc(*"mp4v"),
+                24,
+                self.G.shape[1:],
+                isColor=False,
             )
+
+            frames = np.tile(self.G[0], (self.t - self.gating, 1, 1))
+
+        for i in range(self.t):
+            self.G[-1] = (
+                self.laplassian(-2) * np.power(self.C, 2)
+                + 2 * self.G[-2]
+                - self.G[-3]
+                + self.s(i, self.tp)
+            )
+
+            self.a_scan[i] = self.G[-1, self.tp[0], self.tp[1]]
+
+            if video_out and i >= self.gating:
+                frames[i - self.gating] = self.G[-1]
+
+            self.G = np.roll(self.G, -1, axis=0)
+
+        if video_out:
+            self._write_frames(out, frames)
 
         return self.G
 
-    def a_scan(self) -> np.ndarray:
-        return self.G[:, self.tp[0], self.tp[1]]
-
-    def export_video(self, name: str, fps: int, gating: int):
+    def _write_frames(self, out, frames):
         normalized_grid = (
-            (self.G - np.min(self.G)) * 255 / (np.max(self.G) - np.min(self.G))
-        )
-        frames = cv2.convertScaleAbs(normalized_grid)
-
-        out = cv2.VideoWriter(
-            name,
-            cv2.VideoWriter_fourcc(*"mp4v"),
-            fps,
-            self.G.shape[1:],
-            isColor=False,
+            (frames - np.min(frames)) * 255 / (np.max(frames) - np.min(frames))
         )
 
-        for frame in frames[gating:]:
+        for frame in cv2.convertScaleAbs(normalized_grid):
             out.write(frame)
+
         out.release()
 
 
@@ -65,20 +82,16 @@ if __name__ == "__main__":
     T = 1000
     c = 0.3
 
-    G = np.zeros((T, N, N))
+    G = np.zeros((N, N))
     C = np.full((N, N), c)
-    t = np.arange(T)
-    s = np.zeros_like(G)
 
     mid = int(N / 2)
-    freq = 1 / 50
-    sinusoidal = sinusoidal_source(freq, t)
-    s[:, mid, mid] = sinusoidal
-    simulation = WavePropagation(G, t, C, [mid, mid], s)
-    simulation.simulate()
-    a_scan = simulation.a_scan()
-    import matplotlib.pyplot as plt
 
-    plt.plot(t, a_scan)
-    plt.show()
-    simulation.export_video("kaka.mp4", 24, 0)
+    def source(t: int, coords: tuple[int, int]):
+        freq = 1 / 50
+        s = np.zeros_like(G)
+        s[coords] = sinusoidal_source(freq, t)
+        return s
+
+    simulation = WavePropagation(G, T, C, (mid, mid), source)
+    simulation.simulate(video_out=True)
